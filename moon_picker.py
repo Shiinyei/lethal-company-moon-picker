012e0b6be509c041cf512b84095569d62ee48210
Cyril <cyril-work@pm.me>
2025-06-21 00:01:01 +0200

    Added logic to find suitable moons & pick a random one

--- moon_picker.py
+++ moon_picker.py
@@ -1,14 +1,40 @@
 import argparse
 import os.path
+import random
 
 parser = argparse.ArgumentParser("moon_picker")
 parser.add_argument("-i", "--input", help="The CSV input file from which moon data is read. Each line defines a single moon and must contain two values: the moon's name and its cost respectively. If no input option is provided, 'vanilla.csv' in the current directory will be used instead.")
 parser.add_argument("-c","--credit", help="The current amount of money that your crew possesses. Any moon whose cost exceeds your savings will be ignored so that you are guaranteed to get a moon you can pay for.")
 args = parser.parse_args()
 
-CREDIT = int(0 if args.credit == None else args.credit)
+CREDIT = int(-1 if (args.credit == None or not str(args.credit).isdigit()) else args.credit)
 FILE_PATH = args.input or "vanilla.csv"
 
 if(not os.path.isfile(FILE_PATH)):
   print("❌ File at the specified "+FILE_PATH+" path couldn't be found.")
   quit()
+
+moons = []
+with open(FILE_PATH) as file:
+  for line in file:
+    [moon,cost] = line.split(",")
+
+    if(moon == "" or cost == ""):
+      print("⚠️ Invalid line detected, skipping line...")
+      continue
+
+    cost = cost.replace("\n","").strip()
+    if(not cost.isdigit()):
+      print("⚠️ Cost value (\""+cost+"\") is not a number, skipping line...")
+      continue
+
+    cost = int(cost)
+    if(CREDIT == -1 or CREDIT >= cost):
+      moons.append(moon)
+  
+if(len(moons) == 0):
+  print("❌ No moon could be found in the file. Please check the format and try again...")
+  quit()
+
+moonIndex = random.randint(0,len(moons)-1)
+print("✅🌑 You are going to...   " + moons[moonIndex] + "!")
