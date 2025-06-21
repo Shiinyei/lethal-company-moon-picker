6914496689a10961f876a1347c835214d659d27b
Cyril <cyril-work@pm.me>
2025-06-21 12:13:51 +0200

    Improved handling of optional arguments' default values

--- moon_picker.py
+++ moon_picker.py
@@ -3,12 +3,24 @@ import os.path
 import random
 
 parser = argparse.ArgumentParser("moon_picker")
-parser.add_argument("-i", "--input", help="The CSV input file from which moon data is read. Each line defines a single moon and must contain two values: the moon's name and its cost respectively. If no input option is provided, 'vanilla.csv' in the current directory will be used instead.")
-parser.add_argument("-c","--credit", help="The current amount of money that your crew possesses. Any moon whose cost exceeds your savings will be ignored so that you are guaranteed to get a moon you can pay for.")
+parser.add_argument(
+  "-i",
+  "--input",
+  type=str,
+  default="vanilla.csv",
+  help="The CSV input file from which moon data is read. Each line defines a single moon and must contain two values: the moon's name and its cost respectively. If no input option is provided, 'vanilla.csv' in the current directory will be used instead."
+)
+parser.add_argument(
+  "-c",
+  "--credit",
+  type=int,
+  default="-1",
+  help="The current amount of money that your crew possesses. Any moon whose cost exceeds your savings will be ignored so that you are guaranteed to get a moon you can pay for."
+)
 args = parser.parse_args()
 
-CREDIT = int(-1 if (args.credit == None or not str(args.credit).isdigit()) else args.credit)
-FILE_PATH = args.input or "vanilla.csv"
+CREDIT = args.credit
+FILE_PATH = args.input
 
 if(not os.path.isfile(FILE_PATH)):
   print("❌ File at the specified "+FILE_PATH+" path couldn't be found.")
